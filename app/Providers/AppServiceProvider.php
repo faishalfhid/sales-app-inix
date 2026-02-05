bbff24184b120b9e356d705ea1c929c73b38c3ee
faishalfhid <faishalfh1.work@gmail.com>
2026-02-05 09:35:36 +0700

    Fix NGROK can't load CSS and JS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\TrainingClass;
+use App\Policies\TrainingClassPolicy;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Gate;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,15 +22,21 @@ class AppServiceProvider extends ServiceProvider
     protected $policies = [
         User::class => UserPolicy::class,
         TrainingClass::class => TrainingClassPolicy::class,
+
     ];
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+        public function boot(): void
     {
-        // if (app()->environment('local') || app()->environment('development')) {
-        //     URL::forceScheme('https');
-        // }
+        // Implicitly grant "Super Admin" role all permissions
+        // This works in the app by using gate-related functions like auth()->user->can() and @can()
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
+        if (request()->getHost() && str_contains(request()->getHost(), 'ngrok')) {
+            URL::forceScheme('https');
+        }
     }
 }
